9b2662b1e62b65239dc191f9a069955792c98af2
Kartik Gupta <kartikgupta676@gmail.com>
2022-04-17 11:34:30 -0400

    second commit

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-ryvl3cgj6f@jsnw)y2!j7d#s=ak5va!j547(!d0xxt18cf-f6n')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == '1' # 1 == TRUE
